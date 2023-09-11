203a7315bca527e07cd133d0afb1c510e9e2e028
FabioVasi <vasifabio96@gmail.com>
2023-09-11 14:06:05 +0200

    add img

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ Descrizione:
 Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 
 Bonus:
-Aggiungere alla pagina un’immagine, presa anch’essa inserendo il percoso in una propietá dell'oggetto restituito dalla funzione data.
+Aggiungere alla pagina un’immagine, presa anch’essa inserendo il percoso in una proprietá dell'oggetto restituito dalla funzione data.
 */
 const { createApp } = Vue
 
 createApp({
   data() {
     return {
-      message: 'Hello World!'
+        message: 'Hello World!'
     }
   }
 }).mount('#app')
